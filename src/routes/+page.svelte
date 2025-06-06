e55da728fb376bf208aa64cbbfe5bb6832b8601a
Uncle Common <krishnerkar@gmail.com>
2025-06-06 09:56:18 +0000

    Initial hero generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,15 @@
 	imageSrc="/sample.png"
 	generating
 />
-<LogoScroller label="" generating />
+<LogoScroller
+	label="Trusted by industry leaders"
+	logoUrls={[
+		"https://logo.clearbit.com/ecobank.com",
+		"https://logo.clearbit.com/dangote.com",
+		"https://logo.clearbit.com/sonatel.sn",
+		"https://logo.clearbit.com/totalenergies.com"
+	]}
+/>
 
 <Summary
 	generating
